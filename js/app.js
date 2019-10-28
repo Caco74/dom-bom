2a8f901eed8e24e9a29c42a3ee0fc1ba6395e80e
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2019-10-27 23:51:18 -0300

    vrs 02

--- js/app.js
+++ js/app.js
@@ -1,30 +1,43 @@
-var estudiante = [
+const estudiante = [
 ];
 
 window.onload = cargarEventos;
 
 function cargarEventos() {
+  mostrarTabla()
   document.getElementById('nuevo-estudiante').addEventListener('submit', nuevoEstudiante, false);
 }
 
 function mostrarTabla() {
-  var cuerpoTabla = document.getElementById('estudiante-tabla');
-  var tablaCompleta = "";
-
-  for (var i = 0; i < estudiante.length; i++) {
+  let cuerpoTabla = document.getElementById('estudiante-tabla');
+  let tablaCompleta = "";
+  for (let i = 0; i < estudiante.length; i++) {
     tablaCompleta += "<tr><td>"+estudiante[i].codigo+"</td><td>"+estudiante[i].nombre+"</td><td>"+estudiante[i].nota+"</td></tr>";
   }
   cuerpoTabla.innerHTML = tablaCompleta;
 }
-
 
 function nuevoEstudiante() {
   event.preventDefault();
   let codigo = document.getElementById('codigo-estudiante').value;
   let nombre = document.getElementById('nombre-estudiante').value;
   let nota = document.getElementById('nota-estudiante').value;
-
   let nuevoEstudiante = { codigo: codigo, nombre: nombre, nota: nota };
-  estudiante.push(nuevoEstudiante);
-  mostrarTabla();
+  let busqueda = codigo;
+  let indice = estudiante.findIndex(registro => registro.codigo === busqueda);
+  if (indice != -1) {
+    alert("El estudiante ya EXISTE! Verifique el código.");
+  } else {
+    //alert("No Existe");
+    estudiante.push(nuevoEstudiante);
+    mostrarTabla();
+  }
 }
+
+//console.log("El elemento buscado está en el índice ", indice);
+//activar los errores de span
+//span.style.display = "";
+//span.style.display = "none";
+//*********************************************************
+//  {codigo: "001", nombre: "Franco", nota: "8"},
+// {codigo: "002", nombre: "FRRW", nota: "8"},{codigo: "003", nombre: "Daniel", nota: "8"}
